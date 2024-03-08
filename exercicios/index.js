1f792a72b035bc9666a9efb657bd3f3c6d36d526
Luisa <luisa11sperafico@gmail.com>
2024-03-08 11:08:01 -0300

    commit

---
+++ exercicios/index.js
@@ -0,0 +1,13 @@
+// comando para exibir mensagem 
+
+console.log("O GREMIO VAI SER CAMPEÃO E O INTER NÃO") 
+
+// exercicio d calculos
+
+const num1 = 20;
+const num2= 654;
+
+// soma 
+let soma = num1 + num2;
+
+console.log(" o valor da some é " + soma  ); 
